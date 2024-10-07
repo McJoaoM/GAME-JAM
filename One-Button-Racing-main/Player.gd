1c31fb4c89b80ddd1efb06ab4af3b90fd9419f7a
McJoaoM <130366173+McJoaoM@users.noreply.github.com>
2024-10-07 11:34:33 -0300

    Add files via upload

---
+++ One-Button-Racing-main/Player.gd
@@ -0,0 +1,99 @@
+extends CharacterBody2D
+class_name Player
+
+@export var trail_manager: TrailManager
+@export var angle_displacement: float = 20
+var bias = 0
+@export var speed: int = 300
+@export var angular_speed: float = 0.003
+var direction = Vector2(1,0)
+@export var visual_angle: float = 0.1
+var start_pos: Vector2
+var start_rot: float
+var frameCount: int = 0
+signal collided
+signal finish_collided
+signal switched_direction
+
+
+var going = false
+func start():
+	going = true
+	position = start_pos
+	rotation = start_rot
+	direction = Vector2(1,0)
+func _ready():
+	bias = angle_displacement
+	start_pos = position
+	start_rot = rotation
+	print(start_rot)
+func on_collide():
+	emit_signal("collided")
+	position = start_pos
+	$CollisionShape2D.rotation = start_rot
+	$Sprite2D.rotation = start_rot
+	going = false
+	$Crash.play()
+func _physics_process(delta):
+	frameCount += 1
+	if going:
+		turn()
+		$CollisionShape2D.rotation = direction.angle()+visual_angle*(bias/angle_displacement)
+		$Sprite2D.rotation = direction.angle()+visual_angle*(bias/angle_displacement)
+		velocity = direction*speed
+		if(frameCount%10 == 0):
+			add_point_to_trailmanager()
+		
+		move_and_slide()
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			var rid = collision.get_collider_rid()
+			var collisionLayer = PhysicsServer2D.body_get_collision_layer(rid)
+			#print("I collided with ", collision.get_collider().name)
+			if(collisionLayer % 2 == 1):
+				#var cell = $Tilemap.world_to_map(collision.position - collision.normal)
+				on_collide()
+			print(collisionLayer)
+			if(collisionLayer == 2):
+				finish_collided.emit()
+			
+
+func add_point_to_trailmanager():
+
+	trail_manager.add_point($Sprite2D/LeftWheelAnchor.global_position,$Sprite2D/RightWheelAnchor.global_position)
+
+func turn():
+	direction = direction.rotated(bias*angular_speed)
+
+func switch_direction():
+	bias *= -1
+	switched_direction.emit()
+	$Skid.play()
+func _input(event):
+	if event.is_action_pressed("button"):
+		switch_direction()
+
+extends RigidBody2D
+
+var speed = 200
+
+func _physics_process(delta):
+	var movement = Vector2.ZERO
+	if Input.is_action_pressed("ui_right"):
+		movement.x += 1
+	if Input.is_action_pressed("ui_left"):
+		movement.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		movement.y += 1
+	if Input.is_action_pressed("ui_up"):
+		movement.y -= 1
+	movement = movement.normalized() * speed * delta
+	move_and_slide(movement)
+
+func _on_body_entered(body):
+	if body.name == "Obstacle":
+		vibrate_controller()
+
+func vibrate_controller():
+	OS.vibrate(0.5)  # Duração da vibração em segundos
+

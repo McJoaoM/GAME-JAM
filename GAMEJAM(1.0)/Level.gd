041b48adb9d6ed80ebe2b35463f72aa730aa388b
McJoaoM <130366173+McJoaoM@users.noreply.github.com>
2024-10-09 08:14:58 -0300

    Add files via upload

---
+++ GAMEJAM(1.0)/Level.gd
@@ -0,0 +1,36 @@
+extends Node
+signal player_win
+signal player_fail
+signal back
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_player_collided():
+	$Camera.add_trauma(0.12)
+	emit_signal("player_fail")
+	$CanvasLayer/UI/CountDown.reset_countdown()
+	
+
+
+func _on_player_switched_direction():
+	$Camera.add_trauma(0.07)
+
+
+func _on_player_finish_collided():
+	emit_signal("player_win")
+
+
+func _on_count_down_countdown_ended():
+	$Player.start()
+	$TrailManager.clear()
+
+
+func _on_back_pressed():
+	emit_signal("back")
